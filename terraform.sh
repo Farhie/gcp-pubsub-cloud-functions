2685ba8b396b56a8531501271f2425dc7ce9c386
Adam Fahie <adam.fahie@gmail.com>
2018-01-22 21:10:55 -0800

    Adam - remove back end config

--- terraform.sh
+++ terraform.sh
@@ -10,13 +10,9 @@ fi
 
 TERRAFORM_ACTION=$1
 ENVIRONMENT=$2
-TERRAFORM_STATE_BUCKET=${ENVIRONMENT}-order-discovery-terraform-state
-BACKEND_CONFIG="-backend-config=bucket=${TERRAFORM_STATE_BUCKET} -backend-config=region=us"
 
 export GOOGLE_APPLICATION_CREDENTIALS=~/.google_cloud/account.json
 
-echo "Using state bucket: ${TERRAFORM_STATE_BUCKET}"
-
 cd environment-infrastructure
-terraform init ${BACKEND_CONFIG}
+terraform init
 terraform ${TERRAFORM_ACTION} -var-file=../variables/${ENVIRONMENT}/environment.tfvars
